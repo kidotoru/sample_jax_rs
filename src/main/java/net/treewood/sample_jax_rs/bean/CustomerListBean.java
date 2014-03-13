ce1748889208fda5347157c636e01572a906e66c
tour kido <t-kido@upgarage.com>
2014-03-13 18:04:50 +0900

    不要なコメント、インポートの削除

--- src/main/java/net/treewood/sample_jax_rs/bean/CustomerListBean.java
+++ src/main/java/net/treewood/sample_jax_rs/bean/CustomerListBean.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package net.treewood.sample_jax_rs.bean;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
--- src/main/java/net/treewood/sample_jax_rs/resource/MessageResource.java
+++ src/main/java/net/treewood/sample_jax_rs/resource/MessageResource.java
@@ -1,19 +1,14 @@
 package net.treewood.sample_jax_rs.resource;
 
-import net.treewood.sample_jax_rs.bean.CustomerBean;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.xml.bind.annotation.XmlRootElement;
 import net.treewood.sample_jax_rs.bean.CustomerListBean;
 import net.treewood.sample_jax_rs.service.MessageService;
 
